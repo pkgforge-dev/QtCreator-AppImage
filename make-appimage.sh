85b7356b146a295773a8d0b59bdcc4c890911087
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:29:46 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,3 +17,7 @@ quick-sharun /usr/bin/qtcreator /usr/lib/qtcreator /usr/share/qtcreator /usr/lib
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
